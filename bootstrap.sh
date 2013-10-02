29a7245f66016cfe1b2ea9a3090221d3621dfc54
Alan Braithwaite <abraithwaite@mail.csuchico.edu>
2013-10-02 14:52:46 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,8 @@
 if [ ! $(which git) ];
 then
     echo "Please enter your password to install git"
-    sudo apt-get install git zsh
+    sudo apt-get install -y git
 fi
 git clone --recursive https://github.com/Caustic/dotfiles.git $HOME/.dotfiles
 cd $HOME/.dotfiles && $HOME/.dotfiles/install.sh
+sudo apt-get install -y zsh
